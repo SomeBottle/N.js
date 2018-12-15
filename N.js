42b74107a3250930693c9b3a4e589357ab1909a8
SomeBottle <583797659@qq.com>
2018-12-15 18:51:38 +0800

    🌭 弹幕分div支持~

--- N.js
+++ N.js
@@ -1,28 +1,59 @@
 ﻿var $N = {
-    mcss: '@keyframes Ndm{from{left:120%}to{left:0;transform:translateX(-100%)}}@-webkit-keyframes Ndm{from{left:120%}to{left:0;transform:translateX(-100%)}}@-moz-keyframes Ndm{from{left:120%}to{left:0;transform:translateX(-100%)}}@-o-keyframes Ndm{from{left:120%}to{left:0;transform:translateX(-100%)}}.N-n{position:absolute}.N-d{position:absolute;animation:Ndm 5s;animation-fill-mode:forwards;animation-timing-function:linear;animation-play-state:running;}',
-    dnum: 0,
+    mcss: '@keyframes Ndm{from{left:120%}to{left:0;transform:translateX(-100%)}}@-webkit-keyframes Ndm{from{left:120%}to{left:0;transform:translateX(-100%)}}@-moz-keyframes Ndm{from{left:120%}to{left:0;transform:translateX(-100%)}}@-o-keyframes Ndm{from{left:120%}to{left:0;transform:translateX(-100%)}}.N-tm{-webkit-text-shadow:#000 0.5px 0 0,#000 0 0.5px 0,#000 -0.5px 0 0,#000 0 -0.5px 0;-moz-text-shadow:#000 0.5px 0 0,#000 0 0.5px 0,#000 -0.5px 0 0,#000 0 -0.5px 0;text-shadow:#000 0.5px 0 0,#000 0 0.5px 0,#000 -0.5px 0 0,#000 0 -0.5px 0;}.N-n{position:absolute}.N-d{position:absolute;animation:Ndm 5s;animation-fill-mode:forwards;animation-timing-function:linear;-webkit-animation-fill-mode:forwards;-webkit-animation-timing-function:linear;-moz-animation-fill-mode:forwards;-moz-animation-timing-function:linear;-o-animation-fill-mode:forwards;-o-animation-timing-function:linear;}',
+    dnum: {},
     /*Scrolling-Num*/
-    tnum: 0,
+    tnum: {},
     /*Top-Num*/
-    bnum: 0,
+    bnum: {},
     /*Bottom-Num*/
+    bold: {},
+	/*font weight(100-900)*/
     num: 0,
     /*Main Num*/
-    colour: '#FFF',
+    obnum: {},
+    /*Barrage Num of different containers*/
+    colour: {},
     /*Default Color*/
-    opa: 1,
+    opa: {},
     /*Default Opacity*/
-    wt: 5,
+    el: 'none',
+    /*Controlling Element*/
+    wt: {},
     /*Default Running Time*/
-    ps: false,
+    ps: {},
+    psall: false,
     end: {
         'animation': 'animationend',
         'OAnimation': 'oAnimationEnd',
         'MozAnimation': 'animationend',
         'WebkitAnimation': 'webkitAnimationEnd'
     },
-	/*CSS3 Animation Ending Events For Different Browsers*/
-    md: 'normal',
+    /*CSS3 Animation Ending Events For Different Browsers*/
+    md: {},
+    x: function(e) {
+        var o = this;
+        if (o.s(e)) {
+            o.el = e;
+            o.ps[e] = false;
+            o.colour[e] = '#FFF';
+            o.opa[e] = 1;
+            o.wt[e] = 5;
+            o.dnum[e] = 0;
+            o.tnum[e] = 0;
+            o.bnum[e] = 0;
+            o.obnum[e] = 0;
+            o.bold[e] = 'normal';
+            o.md[e] = 'normal';
+            var statu = 'running';
+            if (!o.s('N-ob' + e)) {
+                var i = o.s('N-dm');
+                var p = document.createElement('style');
+                p.id = 'N-ob' + e;
+                p.innerHTML = '.N-ob' + e + '{animation-play-state:' + statu + ';-webkit-animation-play-state:' + statu + ';-moz-animation-play-state:' + statu + ';-o-animation-play-state:' + statu + ';}';
+                i.appendChild(p);
+            }
+        }
+    },
     center: function(e) {
         /*Set to center*/
         var i = e;
@@ -32,14 +63,14 @@
         i.setAttribute('style', i.style.cssText + '-moz-transform:translateX(-50%);');
         i.setAttribute('style', i.style.cssText + '-o-transform:translateX(-50%);');
     },
-    pf: function(e, t) {
+    pf: function(e, t, c) {
         /*Count for no-scrolling danmus*/
         var ot = this;
         setTimeout(function() {
             var it = t;
             function pm(e) {
                 if (e !== null && e !== undefined) {
-                    if (!ot.ps) {
+                    if (!ot.ps[c] && !ot.psall) {
                         it -= 1;
                     }
                     if (it <= 0) {
@@ -73,93 +104,123 @@
         /*Set Properties*/
         var o = this;
         if (pt == 'color') {
-            o.colour = val;
+            o.colour[o.el] = val;
         } else if (pt == 'opacity') {
-            o.opa = (Number(val) / 100).toFixed(2);
+            o.opa[o.el] = (Number(val) / 100).toFixed(2);
         } else if (pt == 'time') {
-            o.wt = val;
+            o.wt[o.el] = val;
         } else if (pt == 'md') {
             if (['normal', 'top', 'bottom'].indexOf(val) !== -1) {
-                o.md = val;
+                o.md[o.el] = val;
+            }
+        } else if (pt == 'bold') {
+            if (val == 'normal') {
+                val = 400;
+            }
+            val = Number(val);
+            if (val >= 100 && val <= 900) {
+                o.bold[o.el] = val;
             }
         }
     },
-    c: function(e, txt) {
+    c: function(txt) {
         /*Create*/
         var ot = this;
-        var ti = ot.wt;
-        var i = ot.s(e);
-        var d = document.createElement('div');
-        var t = document.createTextNode(txt);
-        d.appendChild(t);
-        var lh = 5 * (i.clientWidth / 180);
-        d.style.fontSize = lh + 'px';
-        d.style.color = ot.colour;
-        d.style.opacity = ot.opa;
-        d.style.animationDuration = ti + 's';
-		i.style.wordBreak='keep-all';
-	    i.style.whiteSpace='nowrap';
-        d.setAttribute('style', d.style.cssText + '-webkit-animation-duration:' + ti + 's;');
-        d.setAttribute('style', d.style.cssText + '-moz-animation-duration:' + ti + 's;');
-        d.setAttribute('style', d.style.cssText + '-o-animation-duration:' + ti + 's;');
-        d.id = 'N-d' + ot.num;
-        i.style.position = 'relative';
-        i.style.overflow = 'hidden';
-        if (ot.md == 'normal') {
-            d.style.top = lh * ot.dnum + 'px';
-            d.className = 'N-d';
-            var et;
-            for (et in ot.end) {
-                if (d.style[et] !== undefined) {
-                    break;
+        var el = ot.el;
+        if (el !== 'none' && ot.s(el)) {
+            var e = el;
+            var ti = ot.wt[el];
+            var i = ot.s(e);
+            var d = document.createElement('div');
+            var t = document.createTextNode(txt);
+            d.appendChild(t);
+            var lh = 5 * (i.clientWidth / 180);
+            d.style.fontSize = lh + 'px';
+            d.style.fontWeight = ot.bold[el];
+            d.style.color = ot.colour[el];
+            d.style.opacity = ot.opa;
+            d.style.animationDuration = ti + 's';
+            i.style.wordBreak = 'keep-all';
+            i.style.whiteSpace = 'nowrap';
+            d.setAttribute('style', d.style.cssText + '-webkit-animation-duration:' + ti + 's;');
+            d.setAttribute('style', d.style.cssText + '-moz-animation-duration:' + ti + 's;');
+            d.setAttribute('style', d.style.cssText + '-o-animation-duration:' + ti + 's;');
+            d.id = 'N-d' + ot.num;
+            i.style.position = 'relative';
+            i.style.overflow = 'hidden';
+            if (ot.md[el] == 'normal') {
+                d.style.top = lh * ot.dnum[el] + 'px';
+                d.className = 'N-tm N-d N-ob' + el;
+                var et;
+                for (et in ot.end) {
+                    if (d.style[et] !== undefined) {
+                        break;
+                    }
                 }
+                d.addEventListener(ot.end[et],
+                function() {
+                    i.removeChild(d);
+                });
+                /*Listen to CSS3*/
+                if (ot.dnum[el] >= (i.clientHeight / lh * 0.8)) {
+                    ot.dnum[el] = 0;
+                } else {
+                    ot.dnum[el] += 1;
+                }
+            } else if (ot.md[el] == 'top') {
+                d.className = 'N-tm N-n';
+                d.style.top = lh * ot.tnum[el] + 'px';
+                if (ot.tnum[el] >= (i.clientHeight / (lh * 2))) {
+                    ot.tnum[el] = 0;
+                } else {
+                    ot.tnum[el] += 1;
+                }
+                ot.center(d);
+                ot.pf(d.id, ti, el);
+            } else if (ot.md[el] == 'bottom') {
+                d.className = 'N-tm N-n';
+                d.style.bottom = lh * ot.bnum[el] + 'px';
+                if (ot.bnum[el] >= (i.clientHeight / (lh * 2))) {
+                    ot.bnum[el] = 0;
+                } else {
+                    ot.bnum[el] += 1;
+                }
+                ot.center(d);
+                ot.pf(d.id, ti, el);
             }
-            d.addEventListener(ot.end[et],
-            function() {
-                i.removeChild(d);
-            });
-            /*Listen to CSS3*/
-            if (ot.dnum >= (i.clientHeight / lh)) {
-                ot.dnum = 0;
-            } else {
-                ot.dnum += 1;
-            }
-        } else if (ot.md == 'top') {
-            d.className = 'N-n';
-            d.style.top = lh * ot.tnum + 'px';
-            if (ot.tnum >= (i.clientHeight / (lh * 2))) {
-                ot.tnum = 0;
-            } else {
-                ot.tnum += 1;
-            }
-            ot.center(d);
-            ot.pf(d.id, ti);
-        } else if (ot.md == 'bottom') {
-            d.className = 'N-n';
-            d.style.bottom = lh * ot.bnum + 'px';
-            if (ot.bnum >= (i.clientHeight / (lh * 2))) {
-                ot.bnum = 0;
-            } else {
-                ot.bnum += 1;
-            }
-            ot.center(d);
-            ot.pf(d.id, ti);
-        }
-        ot.num += 1;
-        i.appendChild(d);
-    },
-    theworld: function() {
-        var i = this.s('N-dm');
-        var statu = 'paused';
-        if (!this.ps) {
-            this.ps = true;
-            var p = document.createElement('style');
-            p.id = 'N-pause';
-            p.innerHTML = '.N-d{animation-play-state:' + statu + ';-webkit-animation-play-state:' + statu + ';-moz-animation-play-state:' + statu + ';-o-animation-play-state:' + statu + ';}';
-            i.appendChild(p);
+            ot.num += 1;
+            ot.obnum[el] += 1;
+            i.appendChild(d);
         } else {
-            this.ps = false;
-            i.removeChild(this.s('N-pause'));
+            console.log('[N]Empty Element.');
+        }
+    },
+    theworld: function(f) {
+        var ot = this;
+        var i = ot.s('N-dm');
+        var statu = 'paused';
+        if (f) {
+            if (!ot.psall && !ot.s('N-pause')) {
+                ot.psall = true;
+                var p = document.createElement('style');
+                p.id = 'N-pause';
+                p.innerHTML = '.N-d{animation-play-state:' + statu + ';-webkit-animation-play-state:' + statu + ';-moz-animation-play-state:' + statu + ';-o-animation-play-state:' + statu + ';}';
+                i.appendChild(p);
+            } else {
+                ot.psall = false;
+                i.removeChild(ot.s('N-pause'));
+            }
+        } else {
+            if (!ot.ps[ot.el] && !ot.s('N-' + ot.el + 'pause')) {
+                ot.ps[ot.el] = true;
+                var p = document.createElement('style');
+                p.id = 'N-' + ot.el + 'pause';
+                p.innerHTML = '.N-ob' + ot.el + '{animation-play-state:' + statu + ';-webkit-animation-play-state:' + statu + ';-moz-animation-play-state:' + statu + ';-o-animation-play-state:' + statu + ';}';
+                i.appendChild(p);
+            } else {
+                ot.ps[ot.el] = false;
+                i.removeChild(ot.s('N-' + ot.el + 'pause'));
+            }
         }
     }
 };
